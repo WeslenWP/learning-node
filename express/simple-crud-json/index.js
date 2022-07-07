8d5ab256f2c9ff414aeb3d9b3fbaa33fbe0f7eb9
Weslen <weslen1905@gmail.com>
2022-07-07 12:09:42 -0300

    Correção body-parser

--- express/simple-crud-json/index.js
+++ express/simple-crud-json/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const userRoute = require('./routes/userRoute');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 userRoute(app);
 
--- express/simple-crud-mysql/index.js
+++ express/simple-crud-mysql/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
-const bodyParser = require("body-parser");
 const Post = require('./models/Post');
 
 /* Config */
@@ -16,8 +15,8 @@ app.engine('handlebars', handlebars.engine({
 app.set('view engine', 'handlebars');
 
 // Body Parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 /**Fim Config */
 
